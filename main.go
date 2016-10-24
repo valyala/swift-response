6750c28ab16bbd20d58259f5ebcd8c3490e283ed
Aliaksandr Valialkin <valyala@gmail.com>
2016-10-24 18:59:55 +0300

    switch from fasthttp to standard net/http

--- main.go
+++ main.go
@@ -3,35 +3,35 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/valyala/fasthttp"
 	"log"
 	"math/rand"
+	"net/http"
 )
 
 func main() {
-	if err := fasthttp.ListenAndServe(":8090", requestHandler); err != nil {
+	if err := http.ListenAndServe(":8090", http.HandlerFunc(requestHandler)); err != nil {
 		log.Fatalf("error in server: %s", err)
 	}
 }
 
-func requestHandler(ctx *fasthttp.RequestCtx) {
-	switch string(ctx.Path()) {
+func requestHandler(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
 	case "/json":
-		jsonHandler(ctx)
+		jsonHandler(w)
 	default:
-		ctx.Error("page not found", fasthttp.StatusNotFound)
+		http.Error(w, "page not found", http.StatusNotFound)
 	}
 }
 
-func jsonHandler(ctx *fasthttp.RequestCtx) {
-	ctx.SetContentType("application/json; charset=utf-8")
+func jsonHandler(w http.ResponseWriter) {
+	w.Header().Set("ContentType", "application/json; charset=utf-8")
 
 	a := make(map[string]int, 10)
 	for i := 0; i < 10; i++ {
 		k := fmt.Sprintf("Test Number %d", i+1)
 		a[k] = rand.Intn(1000)
 	}
-	if err := json.NewEncoder(ctx).Encode(a); err != nil {
+	if err := json.NewEncoder(w).Encode(a); err != nil {
 		log.Printf("error when encoding json: %s", err)
 	}
 }
